1258c030533b7f2ba834bb0339d929e4999d214d
MrGlayga <ibnOualid@proton.me>
2024-10-17 10:44:03 +0200

    Impresion de argumentos

--- Main.java
+++ Main.java
@@ -2,5 +2,10 @@ public class Main
 {
         public static void main (String[] arg){
                System.out.println("Free Palestine");
-}
+               if(args.length==0){
+               System.out.println("No hay argumentos");
+               }else{
+               System.out.println("Hay" + args.length + "argumentos");
+               }
+        }
 }

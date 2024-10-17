102b71c251b98c531ea2d0c9c9a0cc68641439b2
MrGlayga <ibnOualid@proton.me>
2024-10-17 10:59:42 +0200

    For, Try/Catch

--- Main.java
+++ Main.java
@@ -1,11 +1,17 @@
-public class Main
-{
-        public static void main (String[] arg){
-               System.out.println("Free Palestine");
-               if(args.length==0){
-               System.out.println("No hay argumentos");
-               }else{
-               System.out.println("Hay" + args.length + "argumentos");
-               }
+public class Main {
+    public static void main(String[] args) {
+        try {
+            if (args.length == 0) {
+                System.out.println("No hay argumentos");
+            } else {
+                System.out.println("Hay " + args.length + " argumentos");
+            }
+
+            for (int i = 0; i < args.length; i++) { 
+                System.out.println(args[i]);
+            }
+        } catch (Exception e) {
+            System.out.println("Error");
         }
+    }
 }

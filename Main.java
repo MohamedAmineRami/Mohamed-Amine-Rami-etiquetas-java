0a58aa3263c8d96020853a01848401f96884c44a
MrGlayga <ibnOualid@proton.me>
2024-10-24 09:40:21 +0200

    Dos argumentos

--- Main.java
+++ Main.java
@@ -4,6 +4,14 @@ public class Main {
                     int caras = Integer.parseInt(args[0]);
                     int resultado = (int) (Math.random() * caras) + 1;
                     System.out.println("Resultado del dado: " + resultado);
+            }else if (args.length == 2) {
+                    int caras = Integer.parseInt(args[0]);
+                    int numeroDeDados = Integer.parseInt(args[1]);
+                    System.out.println("Resultados de los dados:");
+                    for (int i = 0; i < numeroDeDados; i++) {
+                        int resultado = (int) (Math.random() * caras) + 1;
+                        System.out.println("Dado " + (i + 1) + ": " + resultado);
+                    }
             }
     }
 }

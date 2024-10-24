9f7cd500efb1f07fd5f0269c8703c89abf1e6c52
MrGlayga <ibnOualid@proton.me>
2024-10-24 09:35:41 +0200

    Un argumento

--- Main.java
+++ Main.java
@@ -1,17 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        try {
-            if (args.length == 0) {
-                System.out.println("No hay argumentos");
-            } else {
-                System.out.println("Hay " + args.length + " argumentos");
+        if (args.length == 1) {
+                    int caras = Integer.parseInt(args[0]);
+                    int resultado = (int) (Math.random() * caras) + 1;
+                    System.out.println("Resultado del dado: " + resultado);
             }
-
-            for (int i = 0; i < args.length; i++) { 
-                System.out.println(args[i]);
-            }
-        } catch (Exception e) {
-            System.out.println("Error");
-        }
     }
 }

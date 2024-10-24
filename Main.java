b612a828adedcf8fc3d1a3085252b455618b17fa
MrGlayga <ibnOualid@proton.me>
2024-10-24 09:48:26 +0200

    Control de excepciones

--- Main.java
+++ Main.java
@@ -1,10 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         if (args.length == 1) {
+                try {
                     int caras = Integer.parseInt(args[0]);
                     int resultado = (int) (Math.random() * caras) + 1;
                     System.out.println("Resultado del dado: " + resultado);
-            }else if (args.length == 2) {
+                } catch (NumberFormatException e) {
+                    System.out.println("El argumento debe ser un número entero.");
+                }
+            } else if (args.length == 2) {
+                try {
                     int caras = Integer.parseInt(args[0]);
                     int numeroDeDados = Integer.parseInt(args[1]);
                     System.out.println("Resultados de los dados:");
@@ -12,6 +17,11 @@ public class Main {
                         int resultado = (int) (Math.random() * caras) + 1;
                         System.out.println("Dado " + (i + 1) + ": " + resultado);
                     }
+                } catch (NumberFormatException e) {
+                    System.out.println("Ambos argumentos deben ser números enteros.");
+                }
+            } else {
+                System.out.println("No hay argumentos o el número de argumentos es incorrecto.");
             }
-    }
+   }
 }
